89b909fe240d2936ead8130866c2a2b2df1d0714
Paulus Harman <paulus.otto.harman@gmail.com>
2024-10-14 03:15:53 +0700

    event handler untuk link pratinjau dan button tutup pratinjau

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ const page = {
     about:''
 }
 
+const modal = document.getElementById('modal')
+const btnPreview = document.getElementById('btn-preview')
+const btnClose = document.getElementById('btn-close')
+
+btnPreview.addEventListener('click', event => {
+    event.preventDefault()
+    show('modal')
+})
+
+btnClose.addEventListener('click', event=>{
+    event.preventDefault()
+    hide('modal')
+})
+
+function hide(elementId) {
+    const element = document.getElementById(elementId)
+    element.classList.add('hidden')
+}
+
+function show(elementId) {
+    const element = document.getElementById(elementId)
+    element.classList.remove(['hidden'])
+}
