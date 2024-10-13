29c3bc485f2032009800f7c4d3fb076ca06c493b
Paulus Harman <paulus.otto.harman@gmail.com>
2024-10-14 04:50:53 +0700

    revisi pratinjau

---
+++ asset/scripts/preview.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', ()=>{
+    const { tagline, skills, experiences, contact, about } = JSON.parse(localStorage.getItem('profile'))
+
+
+
+})
--- asset/scripts/script.js
+++ asset/scripts/script.js
@@ -9,10 +9,19 @@ const page = {
 const modal = document.getElementById('modal')
 const btnPreview = document.getElementById('btn-preview')
 const btnClose = document.getElementById('btn-close')
+const previewBox = document.querySelector('object')
+
+window.onhashchange = function() {
+    const sections = document.querySelectorAll('section')
+    sections.forEach(section => section.classList.add('hidden'))
+    const section = document.querySelector(location.hash)
+    section.classList.remove('hidden')
+}
 
 btnPreview.addEventListener('click', event => {
     event.preventDefault()
     show('modal')
+    localStorage.setItem('profile', JSON.stringify(page))
 })
 
 btnClose.addEventListener('click', event=>{
